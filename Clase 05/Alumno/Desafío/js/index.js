970ea142ebff888ea7ee2cb3dfc3e41acb58a09f
Costanza Cucuzza <costanzacucuzza@gmail.com>
2018-06-21 16:57:59 -0300

    Desafio 5

--- Clase 05/Alumno/Desafío/js/index.js
+++ Clase 05/Alumno/Desafío/js/index.js
@@ -1,16 +1,58 @@
-var examResults = [7, 5, 6, 4, 3, 2, 8]
+var students = [{
+        firstName: 'Juan',
+        lastName: 'Pérez',
+        dni: 45678987,
+        email: 'juan@gmail.com'
+    },
+    {
+        firstName: 'Ana',
+        dni: 45678989,
+        email: 'ana@gmail.com'
+    },
+    {
+        firstName: 'Pedro',
+        lastName: 'Mármol',
+        dni: 45678956,
+        email: 'pedro@gmail.com'
+    }
+]
 
-function promedio() {
-    var total = 0
-    for (var i = 0; i < examResults.length; i++) {
-        number = examResults[i]
-        total = total + number
+function Student(firstName, lastName, dni, email) {
+    var id = dni
+
+    this.firstName = firstName
+    this.lastName = lastName
+    this.dni = dni
+    this.email = email
+
+    this.getId = function() {
+        return id
     }
 
-    var result = total / examResults.length
-
-    return result
+    this.getFullName = function() {
+        return (this.firstName || '') + ' ' + (this.lastName || '')
+    }
 }
 
-var prom = promedio(examResults)
-console.log(prom)
+var student
+
+var newStudent
+var newStudents = []
+
+for (var i = 0; i < students.length; i++) {
+    student = students[i]
+    newStudent = new Student(
+        student.firstName,
+        student.lastName,
+        student.dni,
+        student.email
+    )
+    newStudents.push(newStudent)
+}
+
+console.log(students)
+console.log(newStudents)
+
+for (var i = 0; i < newStudents.length; i++) {
+    console.log(newStudents[i].getFullName())
+}
